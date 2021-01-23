f3f820d4c4897b22c2b6684706a27c786c3cb5e4
Muhibullah Khan Kamali <57056374+Muhibullah-09@users.noreply.github.com>
2021-01-23 22:27:41 +0500

    bug fixed

--- backend/lambda/dynamoHandler.ts
+++ backend/lambda/dynamoHandler.ts
@@ -1,12 +1,12 @@
 import { EventBridgeEvent, Context } from 'aws-lambda';
 import * as AWS from 'aws-sdk';
-// var https = require('https');
+var https = require('https');
 
-// const options = {
-//     hostname: '',
-//     path: '',
-//     method: ''
-// };
+const options = {
+    hostname: 'https://webhooks.amplify.us-east-2.amazonaws.com',
+    path: '/prod/webhooks?id=cffe3a92-4f9d-437f-9053-cc8300cce754&token=wRyNwkrQ9cVSvbEzhdAIbG3ekxLEW4JPgAQILubv8E',
+    method: 'POST'
+};
 
 
 const dynamoClient = new AWS.DynamoDB.DocumentClient();
@@ -22,10 +22,10 @@ export const handler = async (event: EventBridgeEvent<string, any>, context: Con
                 TableName: TABLE_NAME,
                 Item: { id: 'mk' + Math.random(), ...event.detail },
             }
-            // const req = https.request(options, (res: any) => {
-            //     res.setEncoding('utf8');
-            // });
-            // req.end();
+            const req = https.request(options, (res: any) => {
+                res.setEncoding('utf8');
+            });
+            req.end();
             await dynamoClient.put(params).promise();
         }
 
